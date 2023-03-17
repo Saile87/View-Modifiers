e88841c6dce5f6db9365701d5eaae8b2452789e9
Elias Breitenbach <elias.b@me.com>
2023-03-17 23:17:05 +0100

    additions

--- View_Modifiers_project3_Day23/View_Modifiers_project3_Day23/ContentView.swift
+++ View_Modifiers_project3_Day23/View_Modifiers_project3_Day23/ContentView.swift
@@ -23,12 +23,88 @@ struct MottoView: View {
     }
 }
 
+
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.white)
+            .background(.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text2: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text2)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func wattermarked(with text: String) -> some View {
+        modifier(Watermark(text2: text))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Button("Hello World") {
+        VStack {
+            Text("HELLO WORLD")
+                .padding()
+                .background(.red)
+                .padding()
+                .background(.blue)
+                .padding()
+                .background(.green)
+                .padding()
+                .background(.yellow)
+//                .titleStyle()
+            Button("Hello World") {
+                print(type(of: self.body))
+            }
+            .frame(width: 200, height: 200)
+            .background(.red)
+            
+            VStack {
+                CapsuleText(text: "Fist")
+                    .foregroundColor(.white)
+                CapsuleText(text: "Second")
+                    .foregroundColor(.black)
+                
+                Color.blue
+                    .frame(width: 300, height: 200)
+                    .wattermarked(with: "Hacking with Elias")
+            }
         }
-        .frame(width: 200, height: 200)
-        .background(.red)
     }
 }
 
